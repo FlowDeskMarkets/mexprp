24c580db06f4a4c944b5f596d229586b965603a5
lynnatflowdesk <lynn.tan@flowdesk.co>
2024-07-13 12:44:27 +0800

    Add supplementary methods

--- src/supplementary.rs
+++ src/supplementary.rs
@@ -10,6 +10,31 @@ pub struct Supplementary<N: Num> {
 	pub map: HashMap<String, HashMap<String, SupplementaryDataType<N>>>
 }
 
+impl <N: Num> Supplementary<N> {
+	/// Create a new Supplementary
+	pub fn new() -> Self {
+		Supplementary {
+			vectors: HashMap::new(),
+			map: HashMap::new()
+		}
+	}
+
+	pub fn add_vector(&mut self, key: String, value: SupplementaryDataType<N>) {
+		let current = self.vectors.get(&key);
+		if current.is_some() && current.unwrap().len() > 0 {
+			let mut current = current.unwrap().clone();
+			current.push(value);
+			self.vectors.insert(key, current);
+		} else {
+			self.vectors.insert(key, vec![value]);
+		}
+	}
+
+	pub fn add_hashmap(&mut self, key: String, value: HashMap<String, SupplementaryDataType<N>) {
+		self.map.insert(key, value);
+	}
+}
+
 /// Supported Data Type for Supplementary
 #[derive(Clone)]
 pub enum SupplementaryDataType<N: Num> {
